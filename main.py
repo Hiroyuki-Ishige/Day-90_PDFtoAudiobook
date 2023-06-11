e47fe25c43dcff91a6f70c3e7af119d5049944b2
ishige <hiroyukiishige@hotmail.com>
2023-06-11 08:07:20 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# TODO set up at google cloud platform
+'''
+Create KEY
+https://console.cloud.google.com/apis/credentials?project=day-90-pdftoauditobook
+
+YouTube tutorial 7:53
+https://youtu.be/izdDHVLc_Z0
+
+'''
+
+#TODO Read text from PDF file
+#TODO send API request
---
+++ reference.py
@@ -0,0 +1,11 @@
+"""
+YouTube: Google text-to-speech
+Google Cloud Speech-To-Text API With Python For Beginners
+
+Google cloud console
+https://cloud.google.com/cloud-console/?utm_source=google&utm_medium=cpc&utm_campaign=japac-VN-all-en-dr-BKWS-all-super-trial-EXA-dr-1605216&utm_content=text-ad-none-none-DEV_c-CRE_658271187563-ADGP_Hybrid%20%7C%20BKWS%20-%20EXA%20%7C%20Txt%20~%20Management%20Tools_Cloud%20Console_google%20cloud%20console_main-KWID_43700076375697242-kwd-55675752867&userloc_9040331-network_g&utm_term=KW_google%20cloud%20console&gclid=CjwKCAjwvpCkBhB4EiwAujULMmq6JPSwSFjhZvmqxdoeOL-z_K2HlUssv6aAK8XRfZPVC3hJt76JixoCuzsQAvD_BwE&gclsrc=aw.ds
+
+Google cloud text-to-speech
+https://cloud.google.com/text-to-speech
+
+"""
