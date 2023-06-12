8baf01846df91b039572b2476b06c4b5c26750b2
ishige <hiroyukiishige@hotmail.com>
2023-06-13 06:48:28 +0700

    Update

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 from gtts import gTTS
 from playsound import playsound
+from PyPDF2 import PdfReader
 
+# PDF to text -------------------------------------------------
+#Open PDF file
+# with open("Google_wiki.pdf", mode="r") as f:
+#     pdfFileobj = f.read()
+
+#Create a pdf reader object
+pdfReader = PdfReader("Google_wiki.pdf")
+
+#Print number of pages in the pdf file
+print(f'Page Number: {len(pdfReader.pages)}')
+
+#create a page object
+pageObj = pdfReader.pages[0]
+
+#extract text from page
+text = pageObj.extract_text()
+
+#display text
+print(text)
+
+# Text to Speech---------------------------------------
 audio ="speech.mp3"
 language ="en"
-
-with open("text.txt", mode="r") as f:
-    text = f.read()
 
 speech = gTTS(
                 text=text,
@@ -14,7 +33,5 @@ speech = gTTS(
 speech.save(audio)
 playsound(audio)
 
-#TODO extract text from PDF file
-#TODO Read text from PDF file
-#TODO send API request
+
 
